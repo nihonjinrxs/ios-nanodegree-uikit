86591e4edcf1096b3410e8d9a894352179880426
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-22 02:08:49 -0400

    Complete and working.

--- DoReMi/DoReMi/ViewController.swift
+++ DoReMi/DoReMi/ViewController.swift
@@ -16,20 +16,36 @@ class ViewController: UIViewController, UITableViewDataSource {
     let cellReuseIdentifier = "MyCellReuseIdentifier"
     
     // Choose some data to show in your table
+    struct Note {
+        let title: String
+        let detail: String
+        init(title: String, detail: String) {
+            self.title = title
+            self.detail = detail
+        }
+    }
     
     let model = [
-        // TODO: Fill this array with data
+        Note(title:"Do", detail:"A deer, a female deer"),
+        Note(title:"Re", detail:"A drop of golden sun"),
+        Note(title:"Mi", detail:"A name I call myself"),
+        Note(title:"Fa", detail:"A long, long way to run"),
+        Note(title:"Sol", detail:"A needle pulling thread"),
+        Note(title:"La", detail:"A note to follow Sol"),
+        Note(title:"Ti", detail:"A drink with jam and bread")
     ]
     
     // Add the two essential table data source methods here
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO: Implement method to return the correct number of rows.
+        return self.model.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier)!
+        cell.textLabel?.text = self.model[indexPath.row].title
+        cell.detailTextLabel?.text = self.model[indexPath.row].detail
+        return cell
     }
-
 }
 
