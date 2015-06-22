420ccc62632a2439927962fa30a329c5dc4471f6
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-22 01:53:20 -0400

    Not working yet -- bombs on forced unwrap of UITableViewCell. Need to debug.

--- FavoriteThings/FavoriteThings/ViewController.swift
+++ FavoriteThings/FavoriteThings/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // Model
     
     let favoriteThings = [
-        //TODO: Fill this array with your favorite things. Then use this collection to populate your table.
+        "Raindrops on roses",
+        "Whiskers on kittens",
+        "Bright copper kettles",
+        "Warm woolen mittens",
+        "Brown paper packages tied up with string"
     ]
 
     // Mark: Table View Data Source Methods
@@ -25,10 +29,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-        // TODO: Implement this method to get the correct row count
-        let placeholderCount = 2
-        return placeholderCount
+        return self.favoriteThings.count
     }
 
     
@@ -40,12 +41,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       
       // TODO: Implement method
       // 1. Dequeue a reusable cell from the table, using the correct “reuse identifier”
+        let cell = tableView.dequeueReusableCellWithIdentifier("FavoriteThingsCell")!
       // 2. Find the model object that corresponds to that row
       // 3. Set the images and labels in the cell with the data from the model object
+        cell.textLabel?.text = favoriteThings[indexPath.row]
       // 4. return the cell.
-        
-        let placeholderCell = UITableViewCell()
-        return placeholderCell
+        return cell
     }
 }
 
